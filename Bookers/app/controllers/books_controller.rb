fcad887b128a0685a9ecf02317e7e2284f9b7b35
tarabafussy <sasabinfussy@gmail.com>
2019-08-17 17:49:03 +0900

    [Finish]4-8

---
+++ Bookers/app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:mess] = "edit successfully"
+    end
+    redirect_to book_path(book)
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      flash[:mess] = "add successfully"
+    redirect_to book_path(book)
+  end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
